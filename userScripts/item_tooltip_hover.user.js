d8e1c236089c560f0f51bd54079272c58e2b2bca
pcrane <trigunshin@gmail.com>
2017-12-04 20:51:02 -0500

    tooltip display on hover as well as click

---
+++ userScripts/item_tooltip_hover.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name Pendoria Tooltip Hover
+// @namespace https://github.com/trigunshin/pendoria
+// @description Add tooltip on hover to Pendoria Items.
+// @homepage https://trigunshin.github.com/pendoria
+// @version 1
+// @downloadURL https://trigunshin.github.io/pendora/userScripts/item_tooltip_hover.user.js
+// @updateURL https://trigunshin.github.io/pendoria/userScripts/item_tooltip_hover.user.js
+// @include https://pendoria.net/game
+// ==/UserScript==
+
+$(document).on('mouseover', 'a[data-item-id]', function (event) {
+    event.stopPropagation();
+    var display_item = $('.display-item');
+    var display_left = $(this).offset().left;
+    var display_top = $(this).offset().top;
+
+    ajaxPost("/inventory/equipment/preview/"+$(this).data("item-id"), function(data) {
+        display_item.html(data);
+
+        display_item.css({
+            width: 'auto',
+            left: display_left + 'px',
+            top: display_top - $('.display-item').height() - 40 + 'px'
+        }).show();
+    });
+});
