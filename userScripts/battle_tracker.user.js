5d5b092d36a538ce06ad80935bb05423ff995082
pcrane <trigunshin@gmail.com>
2017-12-05 22:40:18 -0500

    v2 with drop tracking

--- userScripts/battle_tracker.user.js
+++ userScripts/battle_tracker.user.js
@@ -3,59 +3,86 @@
 // @namespace https://github.com/trigunshin/pendoria
 // @description Output battle stats to JS console. Manually show drops by typing "drops" in console.
 // @homepage https://trigunshin.github.com/pendoria
-// @version 1
+// @version 2
 // @downloadURL https://trigunshin.github.io/pendoria/userScripts/battle_tracker.user.js
 // @updateURL https://trigunshin.github.io/pendoria/userScripts/battle_tracker.user.js
 // @include https://pendoria.net/game
 // ==/UserScript==
 
-const drops = [];
-let trackedBattles = 0;
-let wins = 0;
-let averageLifePercent = 0;
+// moment, $ are already on the page
 
-let statDrops = 0;
-let powerDrops = 0;
-let conDrops = 0;
-let agiDrops = 0;
-let dexDrops = 0
 // https://stackoverflow.com/questions/12636613/how-to-calculate-moving-average-without-keeping-the-count-and-data-total
 // New average = old average * (n-1)/n + new value /n
 
-function battleTracker(data) {
-    trackedBattles += 1;
-    if(data.gaineddrops && data.gaineddrops !== "") {
-        drops.push(data.gaineddrops);
-        console.info(moment().toISOString(), data.gaineddrops);
+const battleStats = {
+    statDrops: 0,
+    powerDrops: 0,
+    agiDrops: 0,
+    dexDrops: 0,
+    conDrops: 0,
+
+    averageLifePercent: 0,
+    wins: 0,
+    trackedBattles: 0,
+
+    drops: 0,
+    spDrops: 0,
+    spDropAvg: 0,
+    rhodiumDrops: 0,
+
+};
+
+const dropAmountRegex = / (\d+) /;
+
+function handleDrops(dropString, stats) {
+    stats.drops += 1; // track before anything else
+    console.info(moment().toISOString(), dropString);
+
+    const matchResult = dropString.match(dropAmountRegex);
+    if(matchResult === null) return;
+
+    const amount = matchResult[1];
+    if(dropString.indexOf("spare parts") >= 0) {
+        stats.spDrops += 1;
+        stats.spDropAvg = stats.spDropAvg * (stats.spDrops-1)/stats.spDrops + amount/stats.spDrops;
+    } else if(dropString.indexOf("rhodium") >= 0) {
+        stats.rhodiumDrops += 1;
     }
+}
+
+function printResults({trackedBattles, drops, averageLifePercent, wins, statDrops, conDrops, powerDrops, agiDrops, dexDrops, spDrops, spDropAvg, rhodiumDrops}, playerLifePercent) {
+    console.info(`battles: ${trackedBattles} avgLife%: ${(averageLifePercent*100).toFixed(2)} life%: ${(playerLifePercent*100).toFixed(2)} win%: ${((wins/trackedBattles) *100).toFixed(2)}
+drop%: ${(drops/trackedBattles*100).toFixed(2)} drops: ${drops} spDrops: ${spDrops} spDropAvg: ${spDropAvg} rhoDrops: ${rhodiumDrops} 
+statDrop%: ${((statDrops/trackedBattles) *100).toFixed(2)} conDrop%: ${((conDrops/trackedBattles) *100).toFixed(2)} powDrop%: ${((powerDrops/trackedBattles) *100).toFixed(2)} agiDrop%: ${((agiDrops/trackedBattles) *100).toFixed(2)} dexDrop%: ${((dexDrops/trackedBattles) *100).toFixed(2)}`);
+}
+
+function battleTracker(data) {
+    battleStats.trackedBattles += 1;
+    if(data.gaineddrops && data.gaineddrops !== "") handleDrops(data.gaineddrops, battleStats);
 
     if(data.gainedAgility) {
-        statDrops += 1;
-        agiDrops += 1;
+        battleStats.statDrops += 1;
+        battleStats.agiDrops += 1;
     }
     if(data.gainedConstitution) {
-        statDrops += 1;
-        conDrops += 1;
+        battleStats.statDrops += 1;
+        battleStats.conDrops += 1;
     }
     if(data.gainedPower) {
-        statDrops += 1;
-        powerDrops += 1;
+        battleStats.statDrops += 1;
+        battleStats.powerDrops += 1;
     }
     if(data.gainedDexterity) {
-        statDrops += 1;
-        dexDrops += 1;
+        battleStats.statDrops += 1;
+        battleStats.dexDrops += 1;
     }
 
-    if(data.victory) wins += 1;
+    if(data.victory) battleStats.wins += 1;
 
     const playerLifePercent = (data.playerLife/data.playerMaxLife);
-    averageLifePercent = averageLifePercent * (trackedBattles-1)/trackedBattles + playerLifePercent/trackedBattles;
+    battleStats.averageLifePercent = battleStats.averageLifePercent * (battleStats.trackedBattles-1)/battleStats.trackedBattles + playerLifePercent/battleStats.trackedBattles;
 
-    console.info(`battles: ${trackedBattles} drops: ${drops.length} avgLife%: ${(averageLifePercent*100).toFixed(2)} life%: ${(playerLifePercent*100).toFixed(2)} win%: ${((wins/trackedBattles) *100).toFixed(2)} 
-statDrop%: ${((statDrops/trackedBattles) *100).toFixed(2)} conDrop%: ${((conDrops/trackedBattles) *100).toFixed(2)} powDrop%: ${((powerDrops/trackedBattles) *100).toFixed(2)} agiDrop%: ${((agiDrops/trackedBattles) *100).toFixed(2)} dexDrop%: ${((dexDrops/trackedBattles) *100).toFixed(2)}
-    `);
-
-
+    printResults(battleStats, playerLifePercent);
 }
 
 let battleTrackerFn = battleTracker;
