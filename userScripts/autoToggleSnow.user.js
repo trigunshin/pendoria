dc3268cff54d033d55c5a83ae75a8d016cb6dc80
pcrane <trigunshin@gmail.com>
2017-12-21 15:42:44 -0500

    auto disable pendoria snow on login for scrooges like me

---
+++ userScripts/autoToggleSnow.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name Pendoria Snow Auto-Disable
+// @namespace https://github.com/trigunshin/pendoria
+// @description Automatically disable snow on login. SCROOOOOGE
+// @homepage https://trigunshin.github.com/pendoria
+// @version 1
+// @downloadURL https://trigunshin.github.io/pendoria/userScripts/autoToggleSnow.user.js
+// @updateURL https://trigunshin.github.io/pendoria/userScripts/autoToggleSnow.user.js
+// @include https://pendoria.net/game
+// ==/UserScript==
+
+$('div#snow').toggle();
